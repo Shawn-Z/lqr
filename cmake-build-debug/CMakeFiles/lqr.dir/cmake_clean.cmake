3a96e3174e31abc42c3f21d637d2b92fa0cc80b5
shawn-z <2120170431@bit.edu.cn>
2019-03-20 14:58:11 +0800

    heha

---
+++ cmake-build-debug/CMakeFiles/lqr.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lqr.dir/src/LQR.cpp.o"
+  "devel/lib/lqr/lqr.pdb"
+  "devel/lib/lqr/lqr"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lqr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
